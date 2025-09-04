65310a4da456a5a9e373e174c395f4518b408b4f
Larissa Nunes <131396340+laralararissa@users.noreply.github.com>
2025-09-03 21:35:31 -0300

    aula4

---
+++ main.py
@@ -0,0 +1,12 @@
+from src.extract import extract
+from src.load import load
+
+
+Extract = extract()
+Load = load()
+
+br = Extract.extract_data("Brazil")
+Load.load_data("universities", "universities_br", br)
+
+it = Extract.extract_data("Italy")
+load.load_data("universities", "universities_it", it)
